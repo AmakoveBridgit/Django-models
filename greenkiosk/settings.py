7b87c96538acecf47b076e342a4f8eac86c1099f
AmakoveBridgit <amakovebridgit@gmail.com>
2023-06-30 17:04:56 +0300

    added feedback section

--- greenkiosk/settings.py
+++ greenkiosk/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inventory','delivery','customer','payment','order','cart'
+    'inventory','delivery','customer','payment','order','cart','feedbacks'
     
 ]
 
